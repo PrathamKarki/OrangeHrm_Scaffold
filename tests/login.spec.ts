3190d0739ac2d76f8055a7c11ff8bc19584d45d1
Pratham Karki <you@example.com>
2026-09-23 14:01:00 +0545

    login test with invalid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,19 @@
 import {test, expect} from '@playwright/test';
 
+// test scenarion 1: Login with valid credentails
 test("Successful login with valid credentials", async({page})=>{
     await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await page.getByPlaceholder('Username').fill('Admin');
     await page.getByPlaceholder('Password').fill('admin123');
     await page.getByRole('button', {name: 'Login'}).click();
     await expect(page).toHaveURL(/dashboard/);
+})
+
+// test scenario 2: Login with invalid credentials 
+test("Login with invalid credentails", async({page})=>{
+    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('wronguser');
+    await page.getByPlaceholder('Password').fill('WrongPassword');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page.getByText('Invalid Credentials')).toBeVisible();
 })
