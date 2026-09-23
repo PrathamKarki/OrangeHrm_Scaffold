ccc45b80737e1fbc7f4ba5c0a23b08cb57583b42
Pratham Karki <you@example.com>
2026-09-23 18:09:17 +0545

    Created complete loginpage object with methods

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -12,4 +12,14 @@ export class LoginPage{
         this.passwordInput = page.getByPlaceholder('Password')
         this.loginButton = page.getByRole('button', {name: 'Login'})
     }
+
+    async goto(){
+        await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    }
+
+    async login(username: string, password: string){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
 }
