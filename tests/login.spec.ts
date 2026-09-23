642fe56a0c708609cd5ae3da52b2be2f67b83eb3
Pratham Karki <you@example.com>
2026-09-23 18:10:10 +0545

    Refactored the login test case with POM structure

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,39 +1,33 @@
 import {test, expect} from '@playwright/test';
+import { LoginPage } from '../src/pages/LoginPage';
 
 // test scenarion 1: Login with valid credentails
 test("Successful login with valid credentials", async({page})=>{
-    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByPlaceholder('Username').fill('Admin');
-    await page.getByPlaceholder('Password').fill('admin123');
-    await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page).toHaveURL(/dashboard/,{ timeout: 15000 });
+   const loginPage = new LoginPage(page);
 
+    await loginPage.goto();
+    await loginPage.login('Admin', 'admin123');
+
+    await expect(page).toHaveURL(/dashboard/, {timeout: 15000});
 })
+
 
 // test scenario 2: Login with invalid credentials 
 test("Login with invalid credentials", async({page})=>{
-    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByPlaceholder('Username').fill('wronguser');
-    await page.getByPlaceholder('Password').fill('WrongPassword');
-    await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page.getByText('Invalid credentials')).toBeVisible({ timeout: 15000 });
+   const invalidLogin = new LoginPage(page);
+   await invalidLogin.goto();
+   await invalidLogin.login('WrongUser', 'WrongPassword');
+
+   await expect(page.getByText('Invalid credentials')).toBeVisible({timeout: 15000});
+   
 })
 
 // test scenarion 3: Login with empty field
 test("Login with empty field", async({page})=>{
-    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByPlaceholder('Username').fill('');
-    await page.getByPlaceholder('Password').fill('');
-    await page.getByRole('button', {name: 'Login'}).click();
+    const emptyLoginField = new LoginPage(page); 
+
+    await emptyLoginField.goto();
+    await emptyLoginField.login('', '');
     await expect(page.getByText('Required').first()).toBeVisible({ timeout: 15000 });
 })
 
-test("logout redirects to login page", async({page})=>{
-    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByPlaceholder('Username').fill('Admin');
-    await page.getByPlaceholder('Password').fill("admin123");
-    await  page.getByRole('button', {name: 'Login'}).click()
-    await  page.locator('.oxd-userdropdown-img').click();
-    await page.getByRole('menuitem', {name: 'Logout'}).click();
-    await expect(page).toHaveURL(/login/, {timeout: 1500})
-}) 
