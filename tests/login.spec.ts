e9e456ca9eb2c79a918a194a28bd2069b5216587
Pratham Karki <you@example.com>
2026-09-23 15:44:44 +0545

    added logout test complete the authentication test suite

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,7 +6,8 @@ test("Successful login with valid credentials", async({page})=>{
     await page.getByPlaceholder('Username').fill('Admin');
     await page.getByPlaceholder('Password').fill('admin123');
     await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page).toHaveURL(/dashboard/);
+    await expect(page).toHaveURL(/dashboard/,{ timeout: 15000 });
+
 })
 
 // test scenario 2: Login with invalid credentials 
@@ -15,7 +16,7 @@ test("Login with invalid credentials", async({page})=>{
     await page.getByPlaceholder('Username').fill('wronguser');
     await page.getByPlaceholder('Password').fill('WrongPassword');
     await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page.getByText('Invalid credentials')).toBeVisible();
+    await expect(page.getByText('Invalid credentials')).toBeVisible({ timeout: 15000 });
 })
 
 // test scenarion 3: Login with empty field
@@ -24,5 +25,15 @@ test("Login with empty field", async({page})=>{
     await page.getByPlaceholder('Username').fill('');
     await page.getByPlaceholder('Password').fill('');
     await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page.getByText('Required').first()).toBeVisible();
+    await expect(page.getByText('Required').first()).toBeVisible({ timeout: 15000 });
 })
+
+test("logout redirects to login page", async({page})=>{
+    await  page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill("admin123");
+    await  page.getByRole('button', {name: 'Login'}).click()
+    await  page.locator('.oxd-userdropdown-img').click();
+    await page.getByRole('menuitem', {name: 'Logout'}).click();
+    await expect(page).toHaveURL(/login/, {timeout: 1500})
+}) 
