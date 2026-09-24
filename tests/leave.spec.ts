9e1cb2f4deade385c3b90b4b70cbdf6db79fed6a
Pratham Karki <you@example.com>
2026-09-24 13:50:24 +0545

    Refactor leave tests to use shared testData object

--- tests/leave.spec.ts
+++ tests/leave.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 import { LoginPage } from '../src/pages/LoginPage';
 import { LeavePage } from '../src/pages/LeavePage';
+import { testData } from '../src/utils/testData';
 
 //test scenario: 
 test("Apply leave shows message when no leave balance available ", async({page})=>{
@@ -8,7 +9,7 @@ test("Apply leave shows message when no leave balance available ", async({page})
     const leavePage = new LeavePage(page);
 
     await loginPage.goto();
-    await loginPage.login('Admin', 'admin123');
+    await loginPage.login(testData.validLogin.username, testData.validLogin.username);
     await expect(page).toHaveURL(/dashboard/, {timeout: 15000});
     await leavePage.goToLeave();
     await leavePage.goToApply();
