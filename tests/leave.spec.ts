3cdda4594e0740d7555abb48d5c1c9eb0de05380
Pratham Karki <you@example.com>
2026-09-24 14:52:08 +0545

    refactored for the leave test with login fixtures

--- tests/leave.spec.ts
+++ tests/leave.spec.ts
@@ -1,18 +1,13 @@
-import {test, expect} from '@playwright/test';
-import { LoginPage } from '../src/pages/LoginPage';
+import {test, expect} from '../src/fixtures/authFixture';
 import { LeavePage } from '../src/pages/LeavePage';
-import { testData } from '../src/utils/testData';
+
 
 //test scenario: 
-test("Apply leave shows message when no leave balance available ", async({page})=>{
-    const loginPage = new LoginPage(page);
-    const leavePage = new LeavePage(page);
+test("Apply leave shows message when no leave balance available ", async({loggedInPage})=>{
+    const leavePage = new LeavePage(loggedInPage);
 
-    await loginPage.goto();
-    await loginPage.login(testData.validLogin.username, testData.validLogin.password);
-    await expect(page).toHaveURL(/dashboard/, {timeout: 15000});
     await leavePage.goToLeave();
     await leavePage.goToApply();
 
-    await expect(page.getByText('No Leave Types with Leave Balance')).toBeVisible({timeout: 15000});
+    await expect(loggedInPage.getByText('No Leave Types with Leave Balance')).toBeVisible({timeout: 15000});
 })
