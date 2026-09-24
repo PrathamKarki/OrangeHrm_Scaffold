54f4f79a2b8ce9574d38c76a67b10e55dba37da0
Pratham Karki <you@example.com>
2026-09-24 10:31:30 +0545

    fix: remove unused hamburger click

--- src/pages/EmployeePage.ts
+++ src/pages/EmployeePage.ts
@@ -2,7 +2,6 @@ import {Page, Locator} from '@playwright/test';
 
 export class EmployeePage{
     readonly page: Page;
-    readonly hamburgerIcon: Locator;
     readonly pimLink: Locator;
     readonly employeeInfoSection : Locator;
     readonly employeeNameInput: Locator;
@@ -10,15 +9,13 @@ export class EmployeePage{
 
     constructor(page: Page){
         this.page = page;
-        this.hamburgerIcon = page.locator('.oxd-topbar-header-hamburger');
         this.pimLink = page.getByRole('link', {name: 'PIM'});
         this.employeeInfoSection = page.getByText('Employee Information');
-        this.employeeNameInput = page.getByPlaceholder('Type for hints...')
+        this.employeeNameInput = page.getByPlaceholder('Type for hints...').first();
         this.searchBtn = page.getByRole('button', {name: 'Search'})
     }
 
       async goToEmployeeList () {
-        await this.hamburgerIcon.click();
         await this.pimLink.click();
     }
 
