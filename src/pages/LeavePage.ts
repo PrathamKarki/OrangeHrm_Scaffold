47d36da49188f898254ffe680458d3079579a2ca
Pratham Karki <you@example.com>
2026-09-24 11:26:40 +0545

    created leavepage object

---
+++ src/pages/LeavePage.ts
@@ -0,0 +1,23 @@
+import {Page, Locator} from '@playwright/test';
+
+export class LeavePage{
+    readonly page: Page;
+    readonly leaveLink: Locator;
+    readonly applyTab: Locator;
+
+
+    constructor(page: Page){
+        this.page = page;
+        this.leaveLink = page.getByRole('link', {name: 'Leave'});
+        this.applyTab = page.getByRole('link', {name: 'Apply'});
+
+    }
+
+    async goToLeave(){
+        await this.leaveLink.click();
+    }
+
+    async goToApply(){
+        await this.applyTab.click();
+    }
+}
