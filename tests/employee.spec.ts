32267e746327270b03776982ac65999aeecb8ad7
Pratham Karki <you@example.com>
2026-09-24 14:45:28 +0545

    refactored code for employee test with resuable login fixture

--- tests/employee.spec.ts
+++ tests/employee.spec.ts
@@ -1,70 +1,57 @@
-import {test, expect} from '@playwright/test';
+import {test, expect} from '../src/fixtures/authFixture';
 import { EmployeePage } from '../src/pages/EmployeePage';
 import { LoginPage } from '../src/pages/LoginPage';
 import { testData } from '../src/utils/testData';
 
 // test scenarion: Employe List page loads successfully 
 
-test("Employee List page loads successfully", async({page})=>{
-    const loginPage = new LoginPage(page);
-    const employeePage = new EmployeePage(page);
+test("Employee List page loads successfully", async({loggedInPage})=>{
+    
+    const employeePage = new EmployeePage(loggedInPage);
 
-    await loginPage.goto();
-    await loginPage.login(testData.validLogin.username, testData.validLogin.password);
     await employeePage.goToEmployeeList();
-    await expect(page).toHaveURL(/viewEmployeeList/)
-})
+    await expect(loggedInPage).toHaveURL(/viewEmployeeList/)
+});
 
 
 
 // test scenario: Search for the available employee
-test("Search for the available employee", async({page})=>{
-    const loginPage = new LoginPage(page);
-    const employeePage = new EmployeePage(page);
+test("Search for the available employee", async({loggedInPage})=>{
+    const employeePage = new EmployeePage(loggedInPage);
     const searchedName = testData.searchEmployee;
 
-    await loginPage.goto();
-    await loginPage.login(testData.validLogin.username, testData.validLogin.password);
     await employeePage.goToEmployeeList();
     await employeePage.searchEmployee(searchedName);
-    await expect(page.getByText('Peter Mac')).toBeVisible({timeout: 15000});
+    await expect(loggedInPage.getByText('Peter Mac')).toBeVisible({timeout: 15000});
 
-
-})
+});
 
 
 // test scenario: View the employee detail page
 
-test("View employee detail page", async({page})=>{
-    const loginPage = new LoginPage(page);
-    const employeePage = new EmployeePage(page);
+test("View employee detail page", async({loggedInPage})=>{
+    const employeePage = new EmployeePage(loggedInPage);
     const searchedName = testData.searchEmployee;
 
-    await loginPage.goto();
-    await loginPage.login(testData.validLogin.username, testData.validLogin.password);
     await employeePage.goToEmployeeList();
     await employeePage.searchEmployee(searchedName);
     await employeePage.openEmployeeDetails();
 
-    await expect(page.getByRole('heading', {name: 'Personal Details'})).toBeVisible({timeout: 15000});
+    await expect(loggedInPage.getByRole('heading', {name: 'Personal Details'})).toBeVisible({timeout: 15000});
     
 })
 
 
 // test scenario: add new employee successfully
-test("Add new employee successfully", async({page})=>{
-    const loginPage = new LoginPage(page);
-    const employeePage = new EmployeePage(page);
+test("Add new employee successfully", async({loggedInPage})=>{
+    const employeePage = new EmployeePage(loggedInPage);
     const firstName = testData.newEmployee.firstName; 
     const lastName = testData.newEmployee.lastName;
 
-    await loginPage.goto();
-    await loginPage.login(testData.validLogin.username, testData.validLogin.password);
-    await expect(page).toHaveURL(/dashboard/, { timeout: 15000 });  
     await employeePage.goToEmployeeList();
 
     await employeePage.addEmployee();
     await employeePage.addEmployeeData(firstName, '', lastName);
 
-    await expect(page).toHaveURL(/viewPersonalDetails/, {timeout: 15000})
+    await expect(loggedInPage).toHaveURL(/viewPersonalDetails/, {timeout: 15000})
 })
